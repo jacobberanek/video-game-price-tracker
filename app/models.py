2b7ade323637579ccb0b2d0faaa8f0a60ccbfd40
Jacob Beranek <jacobberanek@gmail.com>
2026-01-25 12:00:31 -0600

    Added Vendor and Price_History models

--- app/models.py
+++ app/models.py
@@ -1,12 +1,34 @@
-from sqlalchemy import Column, Integer, String
-from pydantic import BaseModel
+from sqlalchemy import Column, Integer, String, Float, DateTime, ForeignKey
+from sqlalchemy.orm import relationship
+from datetime import datetime, timezone
 from app.db import Base
 
 #Database Tables
 class Product(Base):
-    __tablename__ = "Products"
+    __tablename__ = "products"
     id = Column(Integer, primary_key=True)
-    name = Column(String, index=True)
-    category = Column(String)
-    external_id = Column(String, index=True) #will only hold steam game id for now
+    name = Column(String, nullable=False, unique=True, index=True)
+    category = Column(String, nullable=False)
+    steam_id = Column(String, nullable=False, unique=True, index=True)
+    created_at = Column(DateTime, default=lambda: datetime.now(timezone.utc))
+    updated_at = Column(DateTime, default=lambda: datetime.now(timezone.utc))
+
+
+class Vendor(Base):
+    __tablename__ = "vendors"
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False, unique=True, index=True)
+    api_endpoint = Column(String, nullable=False, unique=True)
+    created_at = Column(DateTime, default=datetime.now())
+
+
+class Price_History(Base):
+    __tablename__ = "price_history"
+    id = Column(Integer, primary_key=True)
+    product_id = Column(Integer, ForeignKey("products.id", ondelete="RESTRICT"))
+    vendor_id = Column(Integer, ForeignKey("vendors.id", ondelete="RESTRICT"))
+    created_at = Column(DateTime, default=lambda: datetime.now(timezone.utc))
+    updated_at = Column(DateTime, default=lambda: datetime.now(timezone.utc))
+
+
 
