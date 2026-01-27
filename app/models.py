3344cd1718b6d94a80602dc1539c495a7d97e907
Jacob Beranek <jacobberanek@gmail.com>
2026-01-27 13:35:09 -0600

    Completed Phase 2: Data modeling

--- app/models.py
+++ app/models.py
@@ -9,9 +9,11 @@ class Product(Base):
     id = Column(Integer, primary_key=True)
     name = Column(String, nullable=False, unique=True, index=True)
     category = Column(String, nullable=False)
-    steam_id = Column(String, nullable=False, unique=True, index=True)
+    external_id = Column(String, nullable=False, unique=True, index=True)
     created_at = Column(DateTime, default=lambda: datetime.now(timezone.utc))
-    updated_at = Column(DateTime, default=lambda: datetime.now(timezone.utc))
+    updated_at = Column(DateTime, default=lambda: datetime.now(timezone.utc), onupdate=lambda: datetime.now(timezone.utc))
+
+    prices = relationship("Price_History", back_populates="product")
 
 
 class Vendor(Base):
@@ -19,16 +21,22 @@ class Vendor(Base):
     id = Column(Integer, primary_key=True)
     name = Column(String, nullable=False, unique=True, index=True)
     api_endpoint = Column(String, nullable=False, unique=True)
-    created_at = Column(DateTime, default=datetime.now())
+    created_at = Column(DateTime, default=lambda: datetime.now(timezone.utc))
+    updated_at = Column(DateTime, default=lambda: datetime.now(timezone.utc), onupdate=lambda: datetime.now(timezone.utc))
+
+    prices = relationship("Price_History", back_populates="vendor")
 
 
 class Price_History(Base):
     __tablename__ = "price_history"
     id = Column(Integer, primary_key=True)
+    price = Column(Float, nullable=False)
     product_id = Column(Integer, ForeignKey("products.id", ondelete="RESTRICT"))
     vendor_id = Column(Integer, ForeignKey("vendors.id", ondelete="RESTRICT"))
     created_at = Column(DateTime, default=lambda: datetime.now(timezone.utc))
-    updated_at = Column(DateTime, default=lambda: datetime.now(timezone.utc))
+
+    vendor = relationship("Vendor", back_populates="prices")
+    product = relationship("Product", back_populates="prices")
 
 
 
--- app/schemas.py
+++ app/schemas.py
@@ -1,4 +1,5 @@
 from pydantic import BaseModel
+from datetime import datetime
 
 #Pydantic Schemas
 class ProductCreate(BaseModel):
@@ -8,5 +9,34 @@ class ProductCreate(BaseModel):
 
 class ProductResponse(ProductCreate):
     id: int
+    created_at: datetime
+    updated_at: datetime
+
+    class Config: from_attributes = True
+
+
+
+class VendorCreate(BaseModel):
+    name: str
+    api_endpoint: str
+
+class VendorResponse(VendorCreate):
+    id: int
+    created_at: datetime
+    updated_at: datetime
+
+    class Config: from_attributes = True
+
+
+
+class PriceHistoryCreate(BaseModel):
+    price: float
+    product_id: int
+    vendor_id: int
+
+
+class PriceHistoryResponse(PriceHistoryCreate):
+    id: int
+    created_at: datetime
 
     class Config: from_attributes = True
